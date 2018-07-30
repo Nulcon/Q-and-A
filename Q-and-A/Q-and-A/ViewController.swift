cf60d57b5209c5dc535f42cbb07f58a6e942f9e7
Conner Alegre <cga0@pm.me>
2018-07-30 14:09:45 -0700

    Initial commit

---
+++ Q-and-A/Q-and-A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q-and-A
+//
+//  Created by Conner on 7/30/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
